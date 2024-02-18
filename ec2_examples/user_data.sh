ae5f44b65a42c9caadbe0c7e39e8ee5c529aaa9b
Ibrahim Takouna <ibrahim@gridscale.io>
2024-02-18 10:31:05 +0100

    Add ec2 example

---
+++ ec2_examples/user_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+# Install Apache web server
+# e ->exit immediately exit immediately if any command exits with a non-zero status
+# x ->each command in the script will be printed to the console
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<html><body><h1>Hello World from $(hostname -f) !!</h1></body></html>" >
+/var/www/html/index.html
